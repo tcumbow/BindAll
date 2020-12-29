f90e75d0e1e55a1a634a890f2d5fe82eddde53a4
Tom Cumbow <tcumbow@icloud.com>
2020-12-29 18:35:37 -0500

    new deploy model

---
+++ Deploy.ps1
@@ -0,0 +1,2 @@
+robocopy .\BindAll R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\BindAll /mir
+robocopy .\BindAll "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\BindAll" /mir
--- Sync.ps1
+++
@@ -1,5 +0,0 @@
-unison .\BindAll A:\BindAll -auto -batch
-pause
-unison .\BindAll R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\BindAll -auto -batch
-pause
-unison .\BindAll "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\BindAll" -auto -batch
